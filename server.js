ee3bdbc34da93d1d8517f594b4893f2a3fc94c89
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-01-06 23:26:49 -0300

    add responsible for req.body to work

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require('./routes');
 
 const server = express();
 
+server.use(express.urlencoded({extended: true})) //responsável por fazer funcionar o req.body
 server.use(express.static('public'));
 server.use(routes);
 
