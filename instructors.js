5358734952f33ee6d0d233d00551a4dd0fe195b8
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-01-16 16:02:31 -0300

    working with date

--- instructors.js
+++ instructors.js
@@ -1,5 +1,27 @@
 const fs = require('fs');
 const data = require('./data.json');
+const { age, date } =  require('./utils');
+
+exports.show = function(req, res){
+    const  { id } = req.params;
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id;
+    })
+
+    if(!foundInstructor) return res.send('Instructor not found!');
+
+    
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
+    }
+
+    return res.render('instructors/show', {instructor});
+}
 
 exports.post = function(req, res){
     const keys = Object.keys(req.body);
@@ -9,10 +31,23 @@ exports.post = function(req, res){
             return res.send('Please, fill all fields!')
     }
 
-    req.body.birth = Date.parse(req.body.birth);
-    req.body.created_at = Date.now();
+    let {avatar_url, birth, name, services, gender} = req.body;
 
-    data.instructors.push(req.body);
+    birth = Date.parse(birth);
+    const created_at = Date.now();
+    const id = Number(data.instructors.length + 1);
+
+    
+
+    data.instructors.push({
+        avatar_url,
+        birth, 
+        created_at, 
+        id, 
+        name, 
+        services, 
+        gender
+    });
 
     fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
         if(err) return res.send('Write file error!')
@@ -23,3 +58,19 @@ exports.post = function(req, res){
         //return res.send(req.body)
 }
 
+exports.edit = function(req, res){
+    const  { id } = req.params;
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id;
+    })
+
+    if(!foundInstructor) return res.send('Instructor not found!');
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', { instructor });
+}
